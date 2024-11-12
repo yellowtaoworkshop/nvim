726b4b71b7c3e3367006d30be70fd53cd45cfe89
Gavin Huang <taohua@synopsys.com>
2024-11-12 23:15:51 +0800

    some update from SNPS

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,35 @@
 return {
-  'sainnhe/everforest',
-  dependencies = {'nvim-lualine/lualine.nvim'},
-  lazy = false,
-  priority= 1000,
-  opts = function()
-    vim.opt.background = "dark"
-    -- vim.g.everforest_enable_italic = true
-    vim.g.everforest_background = "hard"
-    vim.cmd.colorscheme("everforest")
-    -- set he cursor color
-  end
+  {
+    'sainnhe/everforest',
+    --enabled = false,
+    dependencies = {'nvim-lualine/lualine.nvim'},
+    lazy = false,
+    priority= 1000,
+    opts = function()
+      vim.opt.background = "dark"
+      -- vim.g.everforest_enable_italic = true
+      vim.g.everforest_background = "soft"
+      vim.cmd.colorscheme("everforest")
+      -- set he cursor color
+    end 
+  },
+
+  {
+    "catppuccin/nvim", 
+    name = "catppuccin", 
+    enabled = false,
+    priority = 1000,
+    opts = function()
+      vim.opt.background = "dark"
+      local opts = {
+        flavour = "auto",
+        backgroud = {
+          light = "latte",
+          darl  = "mocha"
+        }
+      }
+      vim.cmd.colorscheme("catppuccin")
+      return opts
+    end
+  }
 }
